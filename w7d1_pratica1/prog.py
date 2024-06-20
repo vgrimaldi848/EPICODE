18de293e38c4f7d6cdcaf6e1643f03394c20f93a
Vincenzo Grimaldi <69241577+vgrimaldi848@users.noreply.github.com>
2024-06-20 14:19:17 +0200

    wtd1

--- w7d1_pratica1/prog.py
+++ w7d1_pratica1/prog.py
@@ -1,5 +1,8 @@
+
+def getlengthList(wordList):
+  return list(map(lambda x: len(x), wordList))
+
 text = "La tele resta spenta e non la guardo piu ho un nodo in gola che e difficile mandare giu fumo un po sposto via la tenda"
 wordList = text.split(' ')
-lengthList = list(map(lambda x: len(x), wordList))
 print(wordList)
-print(lengthList) 
+print(getlengthList(wordList)) 
