ec38a4032252bea46dcae3f7a86f26263a8a264c
Vincenzo Grimaldi <69241577+vgrimaldi848@users.noreply.github.com>
2024-06-20 14:12:43 +0200

    w7d1_pratica1
    Aggiunto esercizio

---
+++ w7d1_pratica1/prog.py
@@ -0,0 +1,5 @@
+text = "La tele resta spenta e non la guardo piu ho un nodo in gola che e difficile mandare giu fumo un po sposto via la tenda"
+wordList = text.split(' ')
+lengthList = list(map(lambda x: len(x), wordList))
+print(wordList)
+print(lengthList) 
